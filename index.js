17f7359489b1294ef59db367f821b4ea1eba4a9c
Kennedy Mutuku <mutukukennedy5@gmail.com>
2026-01-27 10:19:17 +0300

    Rikuruma Pentecostal Church Nyamira Backend - Rebranded

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/kingdom_enlightenment')
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
 app.get('/', (req, res) => {
   res.send('Kingdom Enlightenment Missions Team Backend is running!');
 });
